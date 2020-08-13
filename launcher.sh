c30b44f18f485cd8adcd3af706de9f52e1df020c
avseng <39445361+avseng@users.noreply.github.com>
2020-08-14 04:04:06 +0530

    script to autorun of voice assistant.

--- launcher.sh
+++ launcher.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 sleep 10
-python3 /home/pi/ai_voice_assistant/ai_voice_assistant/main.py >> /home/pi/ai_voice_assistant/ai_voice_assistant/voice.txt 2>&1
+python3 /home/pi/ai_voice_assistant/main.py >> /home/pi/ai_voice_assistant/voice.txt 2>&1
