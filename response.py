d326ffc1105d65e62a6713f9b33d4102246de777
avseng <39445361+avseng@users.noreply.github.com>
2020-08-24 01:09:23 +0530

    Processing converted text and call action.

--- response.py
+++ response.py
@@ -1,68 +1,81 @@
+"""
+This file process converted text and perform actions accordingly.
+This file can be extended with more action.
+"""
 import valib as va
-import weathercom
 import action as a
 import time
-import os
 
-
-COMMAND_AUDIO_FILE="/mnt/ramdisk/command.wav"
 
 def process_text(text, pa):
-    global time
 
+    """
+    asking who are you?
+    """
     if "who are you" in text:
-        va.audio_playback("i am a i voice assistant system, created by mr. avijit sengupta")
+        va.audio_playback("i am a i voice assistant system")
 
-
-    if "today's weather" in text: 
-        va.audio_playback("which city") 
-        #time.sleep(0.5) 
-        #city = va.recognizeSpeech(COMMAND_AUDIO_FILE, 4) 
-        pa.START_RECORDING = True
-        pa.process(3)
-        city = pa.voice_command_processor()
-        print("City :: "+city)
-        try: 
-            humidity, temp, phrase = a.weatherReport(city) 
-            va.audio_playback("currently in " + city + " temperature is " + str(temp) + " degree celsius, " + "humidity is " + str(humidity) + " percent and sky is " + phrase) 
-            print("currently in " + city + " temperature is " + str(temp) + "degree celsius, " + "humidity is " + str(humidity) + " percent and sky is " + phrase) 
+    """
+    asking about weather information.
+    """
+    if "weather" in text:
+        va.audio_playback("which city")
+        time.sleep(0.5)
+        file_name = pa.process(3)
+        city = pa.voice_command_processor(file_name)
+        print("City :: " + city)
+        try:
+            humidity, temp, phrase = a.weatherReport(city)
+            va.audio_playback(
+                "currently in " + city + " temperature is " + str(temp) + " degree celsius, " + "humidity is " + str(
+                    humidity) + " percent and sky is " + phrase)
+            print("currently in " + city + " temperature is " + str(temp) + "degree celsius, " + "humidity is " + str(
+                humidity) + " percent and sky is " + phrase)
         except KeyError as e:
             va.audio_playback("sorry, i couldn't get the location")
-        #os.remove(COMMAND_AUDIO_FILE)
 
-
-    if "search" in text :
+            
+    """
+    asking for search somthing like:
+    what is raspberry pi
+    who is isac newton etc.
+    """
+    if "search" in text or "Search" in text:
         va.audio_playback("tell me what to search")
-        pa.START_RECORDING = True
-        pa.process(5)
-        search_data=pa.voice_command_processor()
-        #search_data = va.recognizeSpeech(COMMAND_AUDIO_FILE, 4)
+        time.sleep(0.5)
+        file_name = pa.process(5)
+        search_data = pa.voice_command_processor(file_name)
         try:
             result = a.google_search(search_data)
             if result:
                 va.audio_playback(result)
             else:
-                va.audio_playback("sorry, i couldn't find any result for "+search_data)
+                va.audio_playback("sorry, i couldn't find any result for " + search_data)
         except KeyError as e:
-            va.audio_playback("sorry, i couldn't find any result for "+search_data)
+            va.audio_playback("sorry, i couldn't find any result for " + search_data)
             pass
-        #os.remove(COMMAND_AUDIO_FILE)
-    
 
-
-    if "time" in text:
+        
+    """
+    asking aboout current time.
+    """
+    if "time" in text or "Time" in text:
         current_time = a.current_datetime("time")
-        va.audio_playback("right now it is "+current_time)
+        va.audio_playback("right now it is " + current_time)
 
-
-    if "date" in text:
+    """
+    asking about today's date.
+    """
+    if "date" in text or "Date" in text:
         date = a.current_datetime("date")
-        va.audio_playback("today it is "+date)
+        va.audio_playback("today it is " + date)
 
-
-    if "reboot" in text:
+        
+    """
+    asking for rebooting the voice assistant system.
+    """
+    if "reboot" in text or "Reboot" in text:
         va.audio_playback("ok.. rebooting the server")
         a.reboot_server()
-
 
     return "done"
