c09041d325968d4051b8fda6f176aa1aa9b16e90
avseng <39445361+avseng@users.noreply.github.com>
2020-08-30 01:52:46 +0530

    Script to autorun voice assistant after reboot

--- launcher.sh
+++ launcher.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+sleep 3
+sudo alsactl restore -f /etc/asound.state
 sleep 10
-python3 /home/pi/ai_voice_assistant/main.py >> /mnt/ramdisk/voice.txt 2>&1
+python3 /home/pi/ai_voice_assistant/main.py > /mnt/ramdisk/voice.txt 2>&1
+
